ce1b0a2ef55b642178b6dd70dca6e2cfe286e89a
Dhanush2805 <dhanushns2000@gmail.com>
2026-08-10 23:20:43 +0530

    Add user name API

--- api.py
+++ api.py
@@ -3,3 +3,7 @@ from model import User
 
 def get_user(user_id: int) -> User:
     return User(user_id, "Dhanush")
+
+def get_user_name(user_id: int) -> str:
+    user = get_user(user_id)
+    return user.name
