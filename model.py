6e775a1ef6d824ce897e14c18f6600e0d5c8f2c6
Dhanush2805 <dhanushns2000@gmail.com>
2026-08-10 22:55:27 +0530

    modified model.py

--- model.py
+++ model.py
@@ -1,4 +1,10 @@
 class User:
-    def __init__(self, user_id: int, name: str):
+    def __init__(
+        self,
+        user_id: int,
+        name: str,
+        email: str,
+    ):
         self.user_id = user_id
         self.name = name
+        self.email = email
