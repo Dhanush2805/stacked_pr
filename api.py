e49582b84f226a16ad9fa5928276a41245a978e2
Dhanush2805 <91242235+Dhanush2805@users.noreply.github.com>
2026-08-11 20:12:55 +0530

    added api (#2)
    * added api
    
    * Add user name API
    
    * Format user name response
    
    * Add environment config
    
    * Add CI workflow
    
    * Add basic CI test
    
    * Add basic CI test
    
    * Test CI failure propagation
    
    * Test CI failure propagation

---
+++ api.py
@@ -0,0 +1,9 @@
+from model import User
+
+
+def get_user(user_id: int) -> User:
+    return User(user_id, "Dhanush")
+
+def get_user_name(user_id: int) -> str:
+    user = get_user(user_id)
+    return f"User: {user.name}"
