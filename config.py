3ea57ec68385ae9561ad1fe82f9abdefb11c40d2
Dhanush2805 <dhanushns2000@gmail.com>
2026-08-13 23:19:44 +0530

    POC: Added Shared Configuration

---
+++ config.py
@@ -0,0 +1,12 @@
+"""
+Shared configuration used by the application.
+"""
+
+APP_NAME = "Stacked PR Demo"
+APP_VERSION = "1.0"
+
+def get_config():
+    return {
+        "name": APP_NAME,
+        "version": APP_VERSION,
+    }
